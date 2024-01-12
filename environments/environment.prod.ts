df8ebc5b10d2f9cd5004321c421900b814e99cba
dnpwwo <dnpwwo@users.noreply.github.com>
2024-01-12 13:40:01 +1100

    Add files via upload

---
+++ environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  APP_NAME: "Domoticz",
+  API_URL: "API/",
+  WS_Protocol: "ws://",
+  WS_Port: "",
+  WS_URL: "/PUSH",
+  WS_SecProtocol: "domoticz"
+};
